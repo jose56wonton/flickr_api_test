bdca95d7f9453b4c3cea54dfd73f02c462090340
Joshua Wootonn <joshuawootonn@gmail.com>
2017-07-07 17:14:20 -0500

    Initial working tutorial app

---
+++ config/routes.rb
@@ -0,0 +1,4 @@
+Rails.application.routes.draw do
+  resources :photos, only: [:index, :show, :new, :create, :destroy]
+  root 'photos#index'
+end
---
+++ flickraw.rb
@@ -0,0 +1,19 @@
+require 'flickraw'
+
+FlickRaw.api_key="20e9cad7005bf6cde615f962c917a254"
+FlickRaw.shared_secret="ec10e59e21a5c1b9"
+
+token = flickr.get_request_token
+auth_url = flickr.get_authorize_url(token['oauth_token'], :perms => 'delete')
+
+puts "Open this url in your process to complete the authication process : #{auth_url}"
+puts "Copy here the number given when you complete the process."
+verify = gets.strip
+
+begin
+  flickr.get_access_token(token['oauth_token'], token['oauth_token_secret'], verify)
+  login = flickr.test.login
+  puts "You are now authenticated as #{login.username} with token #{flickr.access_token} and secret #{flickr.access_secret}"
+rescue FlickRaw::FailedResponse => e
+  puts "Authentication failed : #{e.msg}"
+end
