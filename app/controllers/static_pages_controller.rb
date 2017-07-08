78f3bde582a4bc8e4a4ebc438da61a3ef3480c44
Joshua Wootonn <joshuawootonn@gmail.com>
2017-07-08 11:54:20 -0500

    Figaro implementation

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,7 @@
 class StaticPagesController < ApplicationController
   require 'flickraw'
   def index
-    set_flickr
+
 
     render_photos
   end
@@ -26,13 +26,5 @@ class StaticPagesController < ApplicationController
       render 'static_pages/index', photos: @photos
     rescue StandardError
       render 'static_pages/unavailable'
-    end
-
-    def set_flickr
-      FlickRaw.api_key = ENV['API_KEY']
-      FlickRaw.shared_secret = ENV['SHARED_SECRET']
-
-      flickr.access_token = ENV['ACCESS_TOKEN']
-      flickr.access_secret = ENV['ACCESS_SECRET']
-    end
+    end  
 end
